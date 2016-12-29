1e83b3b6e15f0b9c1f59b9fa33e8ecaa2a84d05e
Vannak Chhay <vannak.chhay1@gmail.com>
2016-12-28 23:06:39 -0500

    finished code for guessnumber errors for transpiling

--- guessnumber/app/app.module.ts
+++ guessnumber/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { GuessTheNumberComponent } from './guess-the-number.component';
